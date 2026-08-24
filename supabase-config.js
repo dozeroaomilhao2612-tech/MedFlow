16446b66be0eb20dc673656f68d277220c472c5a
dozeroaomilhao2612-tech <dozeroaomilhao2612@gmail.com>
2026-08-24 16:05:30 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,5 +2,5 @@
 // Cole SOMENTE a Project URL e a anon/public key.
 // NUNCA coloque a service_role key aqui.
 
-window.MEDFLOW_SUPABASE_URL = "https://bkntczfttyyyhmmclwp.supabase.co";
+window.MEDFLOW_SUPABASE_URL = "https://bkntczfttyyvyhmmclwp.supabase.co";
 window.MEDFLOW_SUPABASE_ANON_KEY = "sb_publishable_la30HOdBfHvOlltSPXpdng_rJk2qWQE";
